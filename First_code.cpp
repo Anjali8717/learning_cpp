7dfc77e9e089f4c744b28ccdfb09869053da74b5
Student Anjali8717 <Student@anjalimewada8717@gmail.com>
2026-09-06 21:30:25 +0530

    add sum to first code

--- First_code.cpp
+++ First_code.cpp
@@ -1,27 +1,27 @@
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int age = 24;
-//     cout<<sizeof(age)<<endl;
-//     cout<<sizeof("Hello world")<<endl;
-//     return 0;
-// };
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age = 24;
+    cout<<sizeof(age)<<endl;
+    cout<<sizeof("Hello world")<<endl;
+    return 0;
+};
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a = 5, b = 10;
-//     int sum = a + b;
-//     cout<<"sum="<<a+b<<endl;
-//     cout<<"difference="<<a-b<<endl;
-//     cout<<"product="<<a*b<<endl;
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5, b = 10;
+    int sum = a + b;
+    cout<<"sum="<<a+b<<endl;
+    cout<<"difference="<<a-b<<endl;
+    cout<<"product="<<a*b<<endl;
 
-//     return 0;
-// }
+    return 0;
+}
 
-// VOTE criteria 
+VOTE criteria 
 
 #include <iostream>
 using namespace std;
@@ -39,5 +39,68 @@ int main(){
 
     return 0;
 
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter marks : ";
+    cin>>marks;
+
+    if(marks>= 90){
+        cout<<"A";
+    } else if (marks<90 && marks>=80){
+        cout<<"B";
+    }else{
+        cout<<"C";
+    }
+    return 0;
+
+}
+
+Print numbers 1 to 5
+
+// Using while loop
+#include <iostream>
+using namespace std;
+int main(){
+    int count = 1;
+    while(count<=5){
+    cout<< count<< " ";
+    count++;
+    }
+
+    cout<<endl;
+    return 0;
+
+}
+
+USING FOR LOOP
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 10;
+    for(int i  = 1; i<=n; i++){
+    cout<<i<<" ";
+   }
+   cout<< endl;
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    int sum = 0;
+    for(int i = 1; i<=n; i++){
+        sum += i;
+    }
+    
+    cout<<"sum=" <<sum<<endl;
+    
+    return 0;
 }
 
