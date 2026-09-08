b76bd4438f577e04fcdfe5d9298c4c137324a6a3
Student Anjali8717 <Student@anjalimewada8717@gmail.com>
2026-09-08 06:59:58 +0530

    h
     Please enter the commit message for your changes. Lines starting

--- First_code.cpp
+++ First_code.cpp
@@ -104,3 +104,126 @@ int main(){
     return 0;
 }
 
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 10;
+    int i = 1;
+    do{
+        cout<<i << " ";
+        i++;
+    }
+    while(n >= i);
+    cout<<endl;
+    return 0;
+}
+
+Prime no
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 7;
+    bool isPrime = true;
+    for(int i=2; i<=n-1; i++){
+        if(n%i == 0){
+            isPrime == false;
+            break;
+        }
+
+    }
+
+if(isPrime == true){
+    cout<<"Prime no\n";
+ }else{
+    cout<<"Non-prime no\n";
+}
+return 0;
+}
+ 
+SQUARE PATTERN
+#include <iostream> 
+using namespace std;
+int main(){
+    int n= 4;
+    
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            cout<< j << " ";
+        }
+        cout<<endl;    }
+    return 0;
+}
+
+PATTERN ALPHABET
+#include <iostream> 
+using namespace std;
+int main(){
+    int n= 4;
+    
+    for(int i = 1; i<=n; i++){
+        char ch = 'A';
+        for(int j = 1; j<=n; j++) {
+            cout<< ch ;
+            ch = ch + 1;
+        }
+        cout<<endl;    }
+    return 0;
+}
+
+ANOTHER PATTERN
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 3;
+    int num = 1;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<< num<<" ";
+            num++;
+        }
+        cout<<endl;
+    } 
+     return 0;
+}
+
+stars in triangle pattern
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i+1; j++){
+            cout<< "*" <<" ";
+        }
+        cout<<endl;
+    } 
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i+1; j++){
+            cout<<(i+1) <<" ";
+        }
+        cout<<endl;
+    } 
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    char ch = 'A';
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i+1; j++){
+            cout<< char('A'+i) <<" ";
+            ch = ch+1;
+        }
+        cout<<endl;
+    } 
+    return 0;
+}
