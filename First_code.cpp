69f1e91f47d785902d22f21506db7f875daaf3f8
Student Anjali8717 <Student@anjalimewada8717@gmail.com>
2026-09-09 07:36:03 +0530

    changes to be done

--- First_code.cpp
+++ First_code.cpp
@@ -124,7 +124,7 @@ using namespace std;
 int main(){
     int n = 7;
     bool isPrime = true;
-    for(int i=2; i<=n-1; i++){
+    for(int i=2; i<=(n-1); i++){
         if(n%i == 0){
             isPrime == false;
             break;
@@ -225,5 +225,55 @@ int main(){
         }
         cout<<endl;
     } 
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = i+1; j>0; j--){
+            cout<< j <<" ";
+        }
+        cout<<endl;
+    } 
+    return 0;
+}
+
+// INVERTED TRIANGLE PATTERN
+#include <iostream> 
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<i; j++){
+            cout<<" ";            
+        }
+        for(int j = 0; j<n-i; j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// PYRAMID PATTERN
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<(n-i-1); j++){
+            cout<<" ";
+    }
+    for(int j = 1; j<=i+1; j++){
+        cout<<j;
+    }
+    for(int j = i; j>0; j--){
+        cout<<j;
+    }
+    cout<<endl;
+    }
     return 0;
 }
